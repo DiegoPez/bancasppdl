547b27c8b5ae684dd7722f33557488f77a852915
Diegonos PC <diegon0906@gmail.com>
2024-09-11 23:22:11 -0600

    functionalsignin

--- app/src/main/java/com/example/banca2/ApiService.java
+++ app/src/main/java/com/example/banca2/ApiService.java
@@ -7,5 +7,10 @@ import retrofit2.http.POST;
 public interface ApiService {
     @POST("/users/login/")
     Call<LoginResponse> login(@Body LoginRequest loginRequest);
+
+    @POST("/users/signup/")
+    Call<SignupResponse> registerUser(@Body SignupRequest signupRequest);
 }
+
+
 
---
+++ app/src/main/java/com/example/banca2/SignupRequest.java
@@ -0,0 +1,19 @@
+package com.example.banca2;
+
+public class SignupRequest {
+    private String username;
+    private String password;
+    private String email;
+    private String first_name;
+    private String last_name;
+
+    public SignupRequest(String username, String password, String email, String first_name, String last_name) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    // Getters y setters
+}
